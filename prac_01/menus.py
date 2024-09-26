343dff48f4e2f4b2414b8077f743cd359ac14382
jacklovett <jacklovett@mail.com>
2024-09-27 09:24:07 +1000

    Add prac 1 files

---
+++ prac_01/menus.py
@@ -0,0 +1,17 @@
+"""
+Simple menu program
+"""
+
+name = input("Enter name: ")
+print("(H)ello\n(G)oodbye\n(Q)uit")
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print("(H)ello\n(G)oodbye\n(Q)uit")
+    choice = input(">>> ").upper()
+print("Finished.")
