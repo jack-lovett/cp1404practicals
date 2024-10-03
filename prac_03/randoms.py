c2b8bf789f3f6faad94970eee2d960198968345d
jacklovett <jacklovett@mail.com>
2024-10-04 09:27:58 +1000

    Add randoms.py with completed tasks

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+# Line 1
+# Prints a random number inclusively between 5 (smallest) and 20 (largest)
+from random import randint
+
+# Line 2
+# Prints the odd numbers between 3 (inclusive) and 10 (exclusive)
+# Smallest number is 3 and largest number is 9
+# Line 2 can not produce a 4
+
+# Line 3
+# Prints a random float number between 2.5 (smallest) and 5.49 recurring (largest)
+# as it is exclusive of 5.50
+
+# Random number between 1 and 100
+print(randint(1,100))
