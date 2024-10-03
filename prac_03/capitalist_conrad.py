8cdeb6f46728b6e3ff9f18fe7ad96a5e1886fd85
jacklovett <jacklovett@mail.com>
2024-10-04 09:51:21 +1000

    Redirect output to a text file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -16,11 +16,14 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "output.txt"
 
 price = INITIAL_PRICE
 number_of_days = 1
 
-print(f"Starting price: ${price:,.2f}")
+out_file = open(FILENAME, 'w')
+
+print(f"Starting price: ${price:,.2f}", file=out_file)
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -35,5 +38,7 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
     number_of_days += 1
+
+out_file.close()
