f39a56497819ba7c10cb210417d68588b20c517e
jacklovett <jacklovett@mail.com>
2024-10-04 10:39:20 +1000

    Add code to read user's name from file and print with message

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,3 +8,9 @@ name = input("What is your name: ")
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
+
+# 2.
+in_file = open("name.txt", 'r')
+name = in_file.readline().strip()
+print(f"Hi {name}!")
+in_file.close()
