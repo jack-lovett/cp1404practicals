7e2718202462b7a07fba470eb88dc36aa6c2041b
jacklovett <jacklovett@mail.com>
2024-10-04 10:45:37 +1000

    Add code to read first 2 numbers of file and print the sum

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 - Practical
 Reading files
 """
+from prac_03.string_formatting import number
 
 # 1.
 name = input("What is your name: ")
@@ -14,3 +15,12 @@ in_file = open("name.txt", 'r')
 name = in_file.readline().strip()
 print(f"Hi {name}!")
 in_file.close()
+
+# 3.
+with open("numbers.txt", 'r') as file:
+    number_total = 0
+    for i, line in enumerate(file):
+        if i == 2:
+            break
+        number_total += int(line)
+print(number_total)
