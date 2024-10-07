c438d4578c5542545a10fd2546f729d3db406504
jacklovett <jacklovett@mail.com>
2024-10-07 14:18:27 +1000

    Complete files questions

--- prac_03/files.py
+++ prac_03/files.py
@@ -12,24 +12,20 @@ out_file.close()
 
 # 2.
 in_file = open("name.txt", 'r')
-name = in_file.readline().strip()
+name = in_file.read().strip()
 print(f"Hi {name}!")
 in_file.close()
 
 # 3.
 with open("numbers.txt", 'r') as file:
     number_sum = 0
-    for i, line in enumerate(file):
-        if i == 2:
-            break
-        number_sum += int(line)
+    number_sum += int(file.readline())
+    number_sum += int(file.readline())
 print(number_sum)
 
 # 4.
 with open("numbers.txt", 'r') as file:
     number_sum = 0
-    numbers = file.read()
-    number_list = numbers.split('\n')
-    for number in number_list:
-        number_sum += int(number)
+    for line in file:
+        number_sum += int(line)
 print(number_sum)
