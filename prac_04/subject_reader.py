9c8712c7c0c7a9551c31982ecb0c4a73772e2e05
jacklovett <jacklovett@mail.com>
2024-10-11 09:57:04 +1000

    Modify load data function to return nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,6 +14,7 @@ def main():
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +23,10 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts)
         print("----------")
     input_file.close()
+    return data
 
 
 main()
