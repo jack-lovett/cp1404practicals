b84582a5cda60140fc20ed86b8cbb7fb8947740e
jacklovett <jacklovett@mail.com>
2024-10-11 09:37:35 +1000

    Refactor print report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,16 +13,15 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for i, income in enumerate(incomes, start=1):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {i:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
