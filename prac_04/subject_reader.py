b7cb4d64bbc55fa8ee0df996d426db3acb09577b
jacklovett <jacklovett@mail.com>
2024-10-11 10:11:55 +1000

    Remove unnecessary print calls

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -18,15 +18,10 @@ def load_data():
     input_file = open(FILENAME)
     data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
         data.append(parts)
-        print("----------")
     input_file.close()
     return data
 
