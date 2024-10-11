082bf4cc4f9f6b3c469dd14c4da359df03f37688
jacklovett <jacklovett@mail.com>
2024-10-11 19:08:20 +1000

    Add functions for number line generation and display for quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,38 @@
-"""Quick picks generator"""
+"""
+CP1404 Practical
+Quick picks generator
+"""
+
+import random
 
 MINIMUM = 1
 MAXIMUM = 45
 NUMBER_OF_RANDOM_NUMBERS = 6
 
-number_of_quick_picks = int(input("How many quick picks? "))
+
+def main():
+    """Quick picks generator"""
+    number_of_quick_picks = int(input("How many quick picks? "))
+    display_quick_picks(number_of_quick_picks)
+
+
+def display_quick_picks(number_of_quick_picks):
+    """Display quick picks depending on inputted number"""
+    for i in range(number_of_quick_picks):
+        print(generate_unique_number_line())
+
+
+def generate_unique_number_line():
+    """Return sorted, formatted list of unique numbers"""
+    possible_numbers = [number for number in range(MINIMUM, MAXIMUM + 1)]
+    numbers = []
+    for i in range(NUMBER_OF_RANDOM_NUMBERS):
+        random_number = random.choice(possible_numbers)
+        numbers.append(random_number)
+        possible_numbers.remove(random_number)
+    numbers.sort()
+    number_line = " ".join(f"{number:2}" for number in numbers)
+    return number_line
+
+
+main()
