98760f9b9759e2ba2660e3732f63434eff93132c
jacklovett <jacklovett@mail.com>
2024-10-18 09:46:31 +1000

    Complete word occurences exercise

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,4 +1,23 @@
 """
 CP1404/CP5632 Practical
-
+Estimated completion time 10 minutes
 """
+
+text = input("Text: ")
+
+word_to_count = {}
+
+for word in text.split():
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+
+sorted_words_counts = sorted(word_to_count.items())
+
+# Find length of longest word for formatting
+max_length = max(len(word) for word, count in sorted_words_counts)
+
+# Print the sorted word counts with aligned output
+for word, count in sorted_words_counts:
+    print(f"{word:{max_length}} : {count}")
