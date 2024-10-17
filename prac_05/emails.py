e1276a065a0513cd8cc579447adefd3cc79c654d
jacklovett <jacklovett@mail.com>
2024-10-18 09:58:28 +1000

    Complete emails exercise for prac 5

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,3 +2,31 @@
 CP1404/CP5632 Practical
 Estimated completion time 10 minutes
 """
+
+
+def main():
+    email_to_name = {}
+
+    email = input("Email: ").strip()
+    while email != "":
+        name = extract_name_from_email(email).title()
+        correct = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+        if correct not in ["", "y"]:
+            name = input("Name: ").strip().title()
+
+        email_to_name[email] = name
+        email = input("Email: ").strip()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name_from_email(email):
+    """Extracts a name from an email"""
+    username = email.split("@")[0]
+    parts = username.split(".")
+    name = " ".join(parts)
+    return name
+
+
+main()
