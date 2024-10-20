fd80fcc5a76a4375bcc65f05f40541de84a9a4c2
Jack Norton Lovett <jack.nortonlovett@my.jcu.edu.au>
2024-10-20 19:00:13 +1000

    Remove unnecessary comment from word occurrences in prac 5

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -18,6 +18,5 @@ sorted_words_counts = sorted(word_to_count.items())
 # Find length of longest word for formatting
 max_length = max(len(word) for word, count in sorted_words_counts)
 
-# Print the sorted word counts with aligned output
 for word, count in sorted_words_counts:
     print(f"{word:{max_length}} : {count}")
