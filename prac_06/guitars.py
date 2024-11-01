1069ad0ef3349ce40483f5672c1d6151b2ce29a3
jacklovett <jacklovett@mail.com>
2024-11-01 10:14:32 +1000

    Add guitar input and display functionality to guitars in prac 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,3 +4,21 @@ CP1404 Practical - Guitars
 Start time: 9:51am
 End time:
 """
+from prac_06.guitar import Guitar
+
+print("My guitars!")
+
+guitars = []
+
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitars.append(Guitar(name=name, year=year, cost=cost))
+    print(f"{name} ({year}) : ${cost:.2f} added.\n")
+    name = input("Name: ")
+
+print("These are my guitars:")
+for i, guitar in enumerate(guitars, start=1):
+    vintage_string = " (vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
