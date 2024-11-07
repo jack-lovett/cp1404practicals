16a7dba64a15458cc363c70bff55911084e8c81c
jacklovett <jacklovett@mail.com>
2024-11-08 09:48:30 +1000

    Add More Guitars files in prac 7

---
+++ prac_07/guitar.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practical - Guitar
+
+Start time: 9:51am
+End time: 10:24am
+Completion time: 33 minutes
+"""
+
+CURRENT_YEAR = 2024
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
