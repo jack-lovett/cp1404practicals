9feb2fe7239b061c5daa55999d9ee18bfed95ee6
jacklovett <jacklovett@mail.com>
2024-11-08 10:01:21 +1000

    Add guitar reading and display results for testing in prac 7

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -5,12 +5,30 @@ Start time: 9:51am
 End time: 10:24am
 Completion time: 33 minutes
 """
+import csv
 
 CURRENT_YEAR = 2024
 
+IN_FILE = "guitars.csv"
+
+
+def main():
+    guitars = read_guitars()
+    for guitar in guitars:
+        print(guitar)
+
+
+def read_guitars(in_file=IN_FILE):
+    guitars = []
+    with open(IN_FILE, encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
+    return guitars
+
 
 class Guitar:
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         self.name = name
         self.year = year
         self.cost = cost
@@ -18,8 +36,15 @@ class Guitar:
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
+    def __gt__(self, other):
+        return self.year > other.year
+
     def get_age(self):
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
         return self.get_age() >= 50
+
+
+if __name__ == "__main__":
+    main()
