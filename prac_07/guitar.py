4f45f133b9126ebf14710d74aad9485a36b50122
jacklovett <jacklovett@mail.com>
2024-11-08 10:22:51 +1000

    Complete More Guitars task in prac 7

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,35 +1,53 @@
 """
-CP1404 Practical - Guitar
-
-Start time: 9:51am
-End time: 10:24am
-Completion time: 33 minutes
+CP1404 Practical - More Guitars
 """
 import csv
 
 CURRENT_YEAR = 2024
 
-IN_FILE = "guitars.csv"
+CSV_FILE = "guitars.csv"
 
 
 def main():
+    """Guitar """
     guitars = read_guitars()
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name=name, year=year, cost=cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.\n")
+        name = input("Name: ")
     guitars.sort()
+
+    save_guitars(guitars)
     for guitar in guitars:
         print(guitar)
 
 
-def read_guitars(in_file=IN_FILE):
+def read_guitars(in_file=CSV_FILE):
+    """Read guitars from csv file and return a list of objects."""
     guitars = []
-    with open(IN_FILE, encoding="utf-8") as file:
+    with open(CSV_FILE, encoding="utf-8") as file:
         reader = csv.reader(file)
         for row in reader:
             guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
     return guitars
 
 
+def save_guitars(guitars, out_file=CSV_FILE):
+    """Save guitars to csv file."""
+    with open(out_file, mode='w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
+
+
 class Guitar:
+    """Guitar class."""
+
     def __init__(self, name="", year=0, cost=0.0):
+        """Construct Guitar object."""
         self.name = name
         self.year = year
         self.cost = cost
@@ -41,9 +59,11 @@ class Guitar:
         return self.year < other.year
 
     def get_age(self):
+        """Get age of guitar."""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
+        """Determine if a guitar is vintage."""
         return self.get_age() >= 50
 
 
