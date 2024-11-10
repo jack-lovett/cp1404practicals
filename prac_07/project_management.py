0104d25d4c8647879cdc60647f6c32c4c9ff3575
jacklovett <jacklovett@mail.com>
2024-11-10 21:25:03 +1000

    Add save projects function in prac 7

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -22,8 +22,7 @@ def main():
         if choice == "L":
             projects = load_projects()
         elif choice == "S":
-            # save_projects
-            pass
+            save_projects(projects)
         elif choice == "D":
             # display_projects
             pass
@@ -61,6 +60,18 @@ def load_projects(in_file=TXT_FILE):
             name, start_date, priority, cost_estimate, completion_percentage = line.strip().split('\t')
             projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
     return projects
+
+
+def save_projects(projects, out_file=TXT_FILE):
+    with open(out_file, "w", encoding="utf-8") as file:
+        # Write header row
+        file.write("Name\tStart Dsdate\tPriority\tCost Estimate\tCompletion Percentage\n")
+        for project in projects:
+            file.write(
+                f"{project.name}\t{project.start_date}\t"
+                f"{project.priority}\t{project.cost_estimate}\t"
+                f"{project.completion_percentage}\n"
+            )
 
 
 if __name__ == "__main__":
